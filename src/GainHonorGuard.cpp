2cc21d9af21419f5381d486786b2e50061cfd9ea
travsart <travsart27@gmail.com>
2025-01-12 22:23:01 -0500

    up

--- src/GainHonorGuard.cpp
+++ src/GainHonorGuard.cpp
@@ -172,7 +172,7 @@ public:
     //Reward Honor from either a Guard (creature 32768 flag) or Elite kill.
     void RewardHonor(Player* player, Creature* killed)
     {
-        if (!GainHonorGuardEnable || !player->IsAlive() || player->InArena() || player->HasAura(SPELL_AURA_PLAYER_INACTIVE) || player->GetSession()->IsBot())
+        if (!GainHonorGuardEnable || player->GetSession()->IsBot() || !player->IsAlive() || player->InArena() || player->HasAura(SPELL_AURA_PLAYER_INACTIVE))
             return;
         if (!killed || (killed && killed->HasAuraType(SPELL_AURA_NO_PVP_CREDIT)))
             return;
